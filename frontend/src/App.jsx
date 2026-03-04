642489be9aa670d2bca8f90f085feb7257c183e3
evcreton <evan.creton@etu.uca.fr>
2026-03-04 15:28:43 +0100

    feat:app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import CanvasGrid from "./CanvasGrid";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Pixel War</h1>
+      <CanvasGrid />
+    </div>
+  );
+}
