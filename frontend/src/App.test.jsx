a67d999d534aaaba6f781bf27dfd1dc030d9ba4e
lelenglet <lenglet.lea03@gmail.com>
2026-03-11 10:53:45 +0100

    Add tests

---
+++ frontend/src/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from "./App";
+
+describe("Composant App", () => {
+  it("devrait afficher le titre Pixel War", () => {
+    render(<App />);
+    expect(screen.getByText(/Pixel War/i)).toBeInTheDocument();
+  });
+});
